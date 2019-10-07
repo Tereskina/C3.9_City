3b9e651720e8db4d6dbdaf6155e9d74c029229c5
Kseniia Tereskina <39565570+Tereskina@users.noreply.github.com>
2019-10-08 00:12:09 +0100

    Update city.js

--- city.js
+++ city.js
@@ -1,20 +1,17 @@
-city.value = localStorage.getItem('city');
-
 city.oninput = () => {
-    localStorage.setItem('city', city.value)
+	localStorage.setItem('city', city.value)
 };
 
-if (city) { 
+if (localStorage.getItem('city')) { 
 	city.value = localStorage.getItem('city');
 	btn_change.style.display = 'block';
 	city.disabled = true;
 
-
 	btn_change.onclick = () => {
-	localStorage.removeItem('city');
-	city.value='';
-	city.disabled = false;
-	btn_change.style.display = 'none';
+		localStorage.removeItem('city');
+		city.value='';
+		city.disabled = false;
+		btn_change.style.display = 'none';
 	}
 };
 
